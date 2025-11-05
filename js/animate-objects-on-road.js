b0a4d7feea895de8e6135abff6a489002480b904
Line-jpg <lines1310@gmail.com>
2025-11-05 10:34:37 +0100

    Update animate-objects-on-road.js

--- js/animate-objects-on-road.js
+++ js/animate-objects-on-road.js
@@ -1,21 +1,40 @@
 "use strict";
 
-// Hent DOM elementer
-
-const getRedCar = document.getElementById("redCar");
+// hent DOM elementer -
+const getRedCar = document.getElementById("redCar"); // intet . grundet det er et id i html
 const getPoliceCar = document.getElementById("policeCar");
 const getBlueCar = document.getElementById("blueCar");
 
-// Hent også lige baggrund og sol
+const getSun = document.querySelector(".sun"); // . fordi det er en classe i html
+const getScene = document.querySelector(".scene");
 
-const sun = document.querySelector(".sun");
-const scene = document.querySelector(".scene");
-
-// Sæt lyde på bilerne + opretter lyd objekter
+// henter lydfiler
 const soundRedCar = new Audio();
-    soundRedCar.src = "../sound/red-car-horn.wav";
+soundRedCar.src = "../sound/red-car-horn.wav";
 
 const soundPoliceCar = new Audio();
+soundPoliceCar.src = "../sound/police-car-sound.wav";
 
 const soundBlueCar = new Audio();
+soundBlueCar.src = "../sound/blue-car-sound.wav";
 
+// Tilføj advent listeners
+
+if (getRedCar) {
+  getRedCar.addEventListener("click", () => {
+    soundRedCar.play();
+  });
+}
+
+if (getPoliceCar) {
+  getPoliceCar.addEventListener("click", () => {
+    soundPoliceCar.play();
+  });
+}
+
+if (getBlueCar) {
+  getBlueCar.addEventListener("click", () => {
+    soundBlueCar.play();
+
+  });
+}
