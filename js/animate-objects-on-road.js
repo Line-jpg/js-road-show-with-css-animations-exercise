e04032718a7022d2a77c32dfd070d5b69ad8edbe
Line-jpg <lines1310@gmail.com>
2025-11-05 09:56:21 +0100

    opdatering

--- js/animate-objects-on-road.js
+++ js/animate-objects-on-road.js
@@ -1,2 +1,12 @@
 "use strict";
 
+// Hent DOM elementer
+
+const getRedCar = document.getElementById("redCar");
+const getPoliceCar = document.getElementById("policeCar");
+const getBlueCar = document.getElementById("blueCar");
+
+// Hent også lige baggrund og sol
+
+const sun = document.querySelector("sun");
+const scene = document.querySelector("scene");
