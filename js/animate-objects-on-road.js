bf5c538fefd92e1b3d011c8f73b74d08dae62793
Line-jpg <lines1310@gmail.com>
2025-11-05 10:05:31 +0100

    Update animate-objects-on-road.js

--- js/animate-objects-on-road.js
+++ js/animate-objects-on-road.js
@@ -8,5 +8,14 @@ const getBlueCar = document.getElementById("blueCar");
 
 // Hent også lige baggrund og sol
 
-const sun = document.querySelector("sun");
-const scene = document.querySelector("scene");
+const sun = document.querySelector(".sun");
+const scene = document.querySelector(".scene");
+
+// Sæt lyde på bilerne + opretter lyd objekter
+const soundRedCar = new Audio();
+    soundRedCar.src = "../sound/red-car-horn.wav";
+
+const soundPoliceCar = new Audio();
+
+const soundBlueCar = new Audio();
+
